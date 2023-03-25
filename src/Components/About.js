c076c0ed38df0efcaffb4236a67f5538d4705376
shilpa <mulgeshilpa@gmail.com>
2023-03-26 00:13:46 +0530

    About component

---
+++ src/Components/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full  h-screen bg-gradient-to-b from-gray-800 to-black text-white pl-[100px]'>
+    <div  className='w-full flex flex-col justify-center item-center h-full' >
+        <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+            <div className='sm:text-right pb-8 pl-4'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-500'>About</p>
+            </div>
+            <div></div>
+        </div>
+        <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8' >
+            <div className=' sm:text-right text-4xl font-bold'>
+                <p>Hi. I'm Shilpa, nice to meet you. Please take a look around
+                    </p>
+                </div>
+            <div>
+                <p>
+            I'm interested in the Web Development because Web development is a fast-paced, challenging career centered on creative problem-solving and continuous innovation.
+            </p>
+            </div>
+        </div>
+      </div>
+      </div>
+  )
+}
+
+export default About
