eb4f836194772ff918f88d3fea8d903e430b5509
shilpa <mulgeshilpa@gmail.com>
2023-03-26 13:00:36 +0530

    projects

---
+++ src/Components/Projects.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ecommerse from '../assets/ecommerse.jpg'
+import expense from '../assets/expense.jpg';
+import mailbox from '../assets/mailbox.jpg';
+const Projects = () => {
+    const projects=[
+        {
+            id:1,
+            src:ecommerse
+        },
+        {
+            id:2,
+            src:expense
+        },
+        {
+            id:3,
+            src:mailbox
+        }
+    ]
+  return (
+    <div name="projects" className='bg-gradient-to-b from-black
+    to-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col
+        justify-center w-full h-full'>
+            <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4'>Projects</p>
+            <p className='py-6'>Check out some of my work right here</p>
+            </div>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8
+        px-12 sm:px-0'>
+    {
+        projects.map(({id,src})=>(
+
+            <div key={id} className='shadow-md shadow-gray-600 rounded-r-lg'>
+                <img src={src} alt="" className='rounded-md h-80 w-full
+                duration-200 hover:scale-105 '/>
+                <div>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200
+                    hover:scale-105'>Demo</button>
+                    <button className='w-1/2 px-6 py-3 m-4 duration-200
+                    hover:scale-105'>code</button>
+                </div>
+            </div>
+        
+        ))
+    }
+</div>
+        
+        </div>
+    </div>
+  )
+}
+
+export default Projects
